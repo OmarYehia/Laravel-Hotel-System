d30e58d8e42ad3419c039ed3bf45365dbf66cd03
Omar Yehia <oyehia94@gmail.com>
2021-03-26 01:08:42 +0200

    Added permission seeder for the data base

---
+++ database/seeders/PermissionsSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use \App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class PermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // Create permissions
+       Permission::create(['name' => 'approve clients']); // for receptionists and above
+       Permission::create(['name' => 'manage receptionists']); // for managers and above
+       Permission::create(['name' => 'manage floors']); // for managers and above
+       Permission::create(['name' => 'manage rooms']); // for managers and above
+       Permission::create(['name' => 'manage managers']); // for admins
+
+       // Create roles and assigning permissions
+        $adminRole = Role::create(['name' => ' admin']);
+        $adminRole->givePermissionTo('approve clients');
+        $adminRole->givePermissionTo('manage receptionists');
+        $adminRole->givePermissionTo('manage floors');
+        $adminRole->givePermissionTo('manage rooms');
+        $adminRole->givePermissionTo('manage managers');
+
+        $managerRole = Role::create(['name' => 'manager']);
+        $managerRole->givePermissionTo('approve clients');
+        $managerRole->givePermissionTo('manage receptionists');
+        $managerRole->givePermissionTo('manage floors');
+        $managerRole->givePermissionTo('manage rooms');
+
+        $receptionistRole = Role::create(['name' => 'receptionist']);
+        $receptionistRole->givePermissionTo('approve clients');
+
+        // Creating the main admin
+        $user = User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'admin@admin.com',
+            'password' => '123456',
+            'national_id' => '0000000000',
+            'avatar_image' => '/',
+            'created_by' => 1,
+        ]);
+        $user->assignRole($adminRole);
+    }
+}
