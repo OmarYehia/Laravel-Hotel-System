d83c1b6d3b7b68dd3ca4e420ceef58e7c17fa5b9
Omar Yehia <oyehia94@gmail.com>
2021-03-26 22:59:17 +0200

    Added ReservationController

--- app/Http/Controllers/Api/ReservationController.php
+++ app/Http/Controllers/Api/ReservationController.php
@@ -3,9 +3,36 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreReservationRequest;
 
 class ReservationController extends Controller
 {
-    //
+    public function store(StoreReservationRequest $request)
+    {
+        $reservation = Reservation::create($request->all());
+        return $reservation;
+    }
+
+    public function index()
+    {
+        $allReservations = Reservation::with(['client', 'room'])->get();
+        return $allReservations;
+    }
+
+    public function show(Reservation $reservation)
+    {
+        return $reservation;
+    }
+
+    public function destroy(Reservation $reservation)
+    {
+        $reservation->delete();
+        return response()->json(['message' => 'Deleted successfully!']);
+    }
+
+    public function update(StoreReservationRequest $request, Reservation $reservation)
+    {
+        $reservation->update($request->all());
+        return response()->json(['message' => 'Updated successfully!']);
+    }
 }
