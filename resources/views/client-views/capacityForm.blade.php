4121fa160569cd899b60e93fe01957b8cc9314a0
Omar Yehia <oyehia94@gmail.com>
2021-04-03 14:52:54 +0200

    Fixing blade files

--- resources/views/client-views/capacityForm.blade.php
+++ resources/views/client-views/capacityForm.blade.php
@@ -1,15 +1,6 @@
 @extends('layout.main')
 
 @section('title', 'Hotel Transylvania')
-@if(Auth::guard('client')->user()->approval_status==="pending" || Auth::guard('client')->user()->approval_status==="denied")
-
-@section('content-title', 'My Reservations')
-@section('content')
-
-<h1>Your request is still pending, Stay tuned!</h1>
-
-@endsection
-@else
 
 @section('content-title', 'How many persons are with you?')
 
@@ -19,17 +10,17 @@
     <li class="breadcrumb-item active">Capacity Form</li>
 </ol>
 @endsection
+
 @section('content')
-@if(Auth::guard('client')->user()->approval_status=="pending" || Auth::guard('client')->user()->approval_status=="denied" || Auth::guard('user')->user()->banned_at)
+@if(Auth::guard('client')->user()->approval_status=="pending" || Auth::guard('client')->user()->approval_status=="denied" )
 <div class="text-center">
     <h1><b>Your Request is pending approval, Stay tuned</b></h1>
 </div>
-@endsection
 @else
 <form action="{{ route ('ajaxavailablerooms.index') }}" method="GET">
 @csrf
 <input type="text" name="capacity" placeholder="Enter Capacity">
 <input type="submit">
 </form> 
-@endsection
 @endif
+@endsection
